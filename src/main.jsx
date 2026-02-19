ebe2349a7ba30c5d4a8979842ed0248c97318bfe
Gregory <joystickgrindhouse@gmail.com>
2026-02-18 23:55:41 -0800

    Simplify rendering and add error handling in main.jsx
    Refactor main.jsx to simplify rendering and add error handling.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import ErrorBoundary from "./components/ErrorBoundary.jsx";
-import "../assets/styles/main.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+try {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} catch (err) {
+  document.body.innerHTML = `<pre style="color:red;">${err.stack}</pre>`;
+}
